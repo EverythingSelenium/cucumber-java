a8f785ee150d9a664af03ada0595f557e273e936
Gaurang Bharadiya <gaurangpb@gmail.com>
2017-06-05 20:45:57 -0500

    add report, fix read.md

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -5,7 +5,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"},
+        features = {"src/test/resources/features"}
+
+)
 public class RunCukesTest {
 
 }
